0189c2990da4b60154ced3bc6d8f3c57c3089e30
Wand Wong <wand.wong@ibm.com>
2025-05-21 00:32:56 -0400

    increase from 30s to 60s

--- data/data/azure/vnet/main.tf
+++ data/data/azure/vnet/main.tf
@@ -163,16 +163,16 @@ resource "azurerm_role_assignment" "network" {
   principal_id         = azurerm_user_assigned_identity.main.principal_id
 }
 
-resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait_60_seconds" {
   depends_on      = [azurerm_private_endpoint.private_endpoint]
-  create_duration = "30s" 
+  create_duration = "60s" 
 }
 
 # copy over the vhd to cluster resource group and create an image using that
 resource "azurerm_storage_container" "vhd" {
   name                 = "vhd"
   storage_account_name = azurerm_storage_account.cluster.name
-  depends_on           = [time_sleep.wait_30_seconds]
+  depends_on           = [time_sleep.wait_60_seconds]
 }
 
 resource "azurerm_storage_blob" "rhcos_image" {
@@ -182,7 +182,7 @@ resource "azurerm_storage_blob" "rhcos_image" {
   type                   = "Page"
   source_uri             = var.azure_image_url
   metadata               = tomap({ source_uri = var.azure_image_url })
-  depends_on             = [time_sleep.wait_30_seconds]
+  depends_on             = [time_sleep.wait_60_seconds]
 }
 
 # Creates Shared Image Gallery
