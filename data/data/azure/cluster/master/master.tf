4708936c8ecba93789c37f251ebdd8bf797826af
Wand Wong <wand.wong@ibm.com>
2025-07-03 05:41:26 -0400

    add timeout to try make Azure test deploy success

--- data/data/azure/cluster/master/master.tf
+++ data/data/azure/cluster/master/master.tf
@@ -156,5 +156,12 @@ resource "azurerm_linux_virtual_machine" "master" {
     storage_account_uri = null # null enables managed storage account for boot diagnostics
   }
 
+  timeouts {
+    create = "2h"
+    read = "30m"
+    update = "2h"
+    delete = "2h"
+  }
+
   tags = var.azure_extra_tags
 }
