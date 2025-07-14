1f4898419fd4b0adeb249c5802d4a3f174f3d2e0
Wand Wong <wand.wong@ibm.com>
2025-07-14 13:11:43 -0400

    remove non-identifying attribute from the existng vnet link

--- data/data/azure/vnet/common.tf
+++ data/data/azure/vnet/common.tf
@@ -49,5 +49,5 @@ data "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   resource_group_name   = var.azure_network_resource_group_name
   private_dns_zone_name = data.azurerm_private_dns_zone.private_dns_zone.name
   virtual_network_id    = local.virtual_network_id
-  registration_enabled  = false
+  // registration_enabled  = false
 }
